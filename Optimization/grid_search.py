5579bf6d09c8c46a970c7b5f4a958fa1586d5b3a
Hiba OUHMAD <hibaouhmad12@gmail.com>
2026-04-05 22:09:25 +0100

    Update grid_search.py

--- Optimization/grid_search.py
+++ Optimization/grid_search.py
@@ -1,25 +1,31 @@
 import itertools
-from helpers import evaluate_model 
+import numpy as np
+from Optimization.helpers import evaluate_model
+
 def generate_combinations(param_grid):
-    keys = param_grid.keys()
-    values = param_grid.values()
+    keys = list(param_grid.keys())
+    values = list(param_grid.values())
     for combo in itertools.product(*values):
         yield dict(zip(keys, combo))
 
 class GridSearch:
-    def __init__(self, model_class, param_grid, kf, metric):
+    def __init__(self, model_class, param_grid, kf, metric=None):
         self.model_class = model_class
         self.param_grid = param_grid
         self.kf = kf
         self.metric = metric
+        self.best_score  = -np.inf
+        self.best_params = {}
+        self.best_estimator_ = None
     def fit(self, X, y):
-        best_score = -np.inf
-        best_params = None
         for params in generate_combinations(self.param_grid):
             model = self.model_class(**params)
             score = evaluate_model(model, X, y, self.kf, self.metric)
-            if score > best_score:
-                best_score = score
-                best_params = params
-        self.best_score = best_score
-        self.best_params = best_params
+            if score > self.best_score:
+                self.best_score      = score
+                self.best_params     = params
+                self.best_estimator_ = self.model_class(**params)
+        # refit best on full data
+        if self.best_estimator_ is not None:
+            self.best_estimator_.fit(X, y)
+        return self
